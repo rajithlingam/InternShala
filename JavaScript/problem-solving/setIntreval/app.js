32e04bad98811f30e0289d1c9a1085cbe0d63c2b
rajithlingam <rajithlingamedu@gmail.com>
2025-07-06 19:10:12 +0530

    setIntreval

--- JavaScript/problem-solving/setIntreval/app.js
+++ JavaScript/problem-solving/setIntreval/app.js
@@ -1,19 +1,19 @@
-const img = document.getElementById('slider-img');
-const images = [
-  "./assets/img-1.jpg",
-  "./assets/img-2.jpg",
-  "./assets/img-3.jpg",
-  "./assets/img-4.jpg",
-  "./assets/img-5.jpg"
-];
+// const img = document.getElementById('slider-img');
+// const images = [
+//   "./assets/img-1.jpg",
+//   "./assets/img-2.jpg",
+//   "./assets/img-3.jpg",
+//   "./assets/img-4.jpg",
+//   "./assets/img-5.jpg"
+// ];
 
-let index = 0;
+// let index = 0;
 
-setInterval(() => {
-  index = (index + 1) % images.length; 
-  img.style.opacity = 0; 
-  setTimeout(() => {
-    img.src = images[index];
-    img.style.opacity = 1;
-  }, 300); 
-}, 2000);
+// setInterval(() => {
+//   index = (index + 1) % images.length; 
+//   img.style.opacity = 0; 
+//   setTimeout(() => {
+//     img.src = images[index];
+//     img.style.opacity = 1;
+//   }, 300); 
+// }, 2000);
