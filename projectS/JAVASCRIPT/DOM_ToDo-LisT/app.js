3eb4d28a2ba57786d452cf158df9037ee24a8653
rajithlingam <rajithlingamedu@gmail.com>
2025-07-16 21:26:48 +0530

    DOM TODO LIST MINI PROJECT completed

--- projectS/JAVASCRIPT/DOM_ToDo-LisT/app.js
+++ projectS/JAVASCRIPT/DOM_ToDo-LisT/app.js
@@ -5,24 +5,22 @@ const recordList = document.querySelector(".record");
 addBtn.addEventListener("click", listItemOfToDO);
 
 function listItemOfToDO() {
-  // console.log( "test" );
-  // console.log(txtArea.value);
-
   const todoText = txtArea.value.trim();
   if (!todoText) return;
 
   const recordItem = document.createElement("div");
   recordItem.classList.add("record-item-container");
 
-  recordItem.innerHTML = `<span class="record-list">${txtArea.value}</span>
-        <span class="record-but">
-          <button class="record-but-check">&#128505;</button>
-          <button class="record-but-cross">&#10540;</button>
-        </span>
-        <span class="delete-btn">
-        <button class="record-but-delete">&#9940;</button>
-        </span>
-        `;
+  recordItem.innerHTML = `
+    <span class="record-list">${todoText}</span>
+    <span class="record-but">
+      <button class="record-but-check">&#128505;</button>
+      <button class="record-but-cross">&#10540;</button>
+    </span>
+    <span class="delete-btn">
+      <button class="record-but-delete">&#9940;</button>
+    </span>
+  `;
 
   recordItem
     .querySelector(".record-but-delete")
@@ -34,7 +32,7 @@ function listItemOfToDO() {
     .querySelector(".record-but-cross")
     .addEventListener("click", () => {
       const actionSpan = recordItem.querySelector(".record-but");
-      actionSpan.innerHTML = `<p class="not-done">Task NOt Completed</p>`;
+      actionSpan.innerHTML = `<p class="not-done">Task Not Completed</p>`;
     });
 
   recordItem
