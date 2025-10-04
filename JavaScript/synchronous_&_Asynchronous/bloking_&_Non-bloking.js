189d9a9f6fad8a0b29ce1fc38e24876b716c468b
rajithlingam <rajithlingamedu@gmail.com>
2025-10-04 05:51:14 +0530

    node I/O

---
+++ JavaScript/synchronous_&_Asynchronous/bloking_&_Non-bloking.js
@@ -0,0 +1,27 @@
+/*Synchronous || Blocking */
+let num = (numBlock) => {
+  const userData = {
+    1: { name: "jhon", age: "41", id: "1" },
+    2: { name: "smith", age: "26", id: "2" },
+  };
+  return userData[numBlock];
+};
+
+let getBlock = num(2);
+console.log(getBlock);
+
+/* Asynchronous || Non-Blocking */
+
+let numb = (aNumBlock, callBack) => {
+  const userDetail = {
+    1: { name: "jhon", age: "41", id: "1" },
+    2: { name: "smith", age: "26", id: "2" },
+  };
+  setTimeout(() => {
+    callBack(userDetail[aNumBlock]);
+  }, 2000);
+};
+
+numb(1, (userCall) => {
+  console.log(userCall);
+});
